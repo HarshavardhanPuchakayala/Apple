a2980d359b46673eae9addb7a0e6d4bdee49969f
Harshavardhan Puchakayala <85436195+HarshavardhanPuchakayala@users.noreply.github.com>
2024-05-19 17:38:53 +0530

    Completed
    The Project was completed.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Navabar from './components/Navabar'
 import Highlights from './components/Highlights'
 import Hero from './components/Hero'
+import Model from "./components/Model"
+import Features from './components/Features'
+import Game from "./components/Game"
+import Footer from "./components/footer"
 export default function App() {
   return (
     <main className="bg-black">
-<Navabar/>
-<Hero/>
-<Highlights/>
+        <Navabar/>
+        <Hero/>
+        <Highlights/>
+        <Model/>
+        <Features/>
+        <Game/>
+        <Footer/>
     </main>
   )
 }
